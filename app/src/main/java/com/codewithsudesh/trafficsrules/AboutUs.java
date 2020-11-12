a527f3abc361b71706a77b26f6acdb5f878a757a
sudeshkunwar@outlook.com <sudeshkunwar@outlook.com>
2020-11-12 22:02:55 +0545

    AboutUs section added

--- app/src/main/java/com/codewithsudesh/trafficsrules/AboutUs.java
+++ app/src/main/java/com/codewithsudesh/trafficsrules/AboutUs.java
@@ -20,6 +20,38 @@ public class AboutUs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
+        // Setting the Up button in Activity to return to More fragment
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Setting  title to the Activity
+        getSupportActionBar().setTitle(R.string.title_aboutus);
+
+
+        // Image-slider
+        ImageSlider imageSlider = findViewById(R.id.slider);
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.homei));
+        slideModels.add(new SlideModel(R.drawable.homeii));
+        slideModels.add(new SlideModel(R.drawable.homeiii));
+        slideModels.add(new SlideModel(R.drawable.homeiv));
+        imageSlider.setImageList(slideModels, true);
+
+    }
+
+
+
+
+    // Setting the Up button in Activity to return to More fragment
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
 
 
 }
