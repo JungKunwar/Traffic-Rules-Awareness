d1975f63842221fecbd027614591001f59cdbec0
sudeshkunwar@outlook.com <sudeshkunwar@outlook.com>
2020-11-23 22:05:25 +0545

    ContactUs section added

--- app/src/main/java/com/codewithsudesh/trafficsrules/ContactUs.java
+++ app/src/main/java/com/codewithsudesh/trafficsrules/ContactUs.java
@@ -20,5 +20,68 @@ import mehdi.sakout.aboutpage.Element;
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_contact_us);
 
+            // Setting  title to the Activity
+            getSupportActionBar().setTitle(R.string.title_contactus);
+
+
+            // Setting the Up button in Activity to return to More fragment
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            // About Us //
+
+            Element element = new Element();
+            element.setTitle("Advertise here");
+
+            View aboutpage = new AboutPage(this)
+                    .isRTL(false)
+//                .setImage(R.drawable.about_bg)
+                    .setDescription("Welcome To Traffic Rules Awareness")
+                    .addItem(new Element().setTitle("Version 1.0"))
+                    .addItem( element)
+                    .addGroup("Connect With Us")
+                    .addEmail("sudesh6kunwar@gmail.com")
+                    .addWebsite("https://traffic.nepalpolice.gov.np/")
+                    .addFacebook("NepalPolicePHQ")
+                    .addYoutube("UCHbQpMEaQjKW961qPjKe0-A")
+                    .addInstagram("nepalpolice")
+                    .addGitHub("JungKunwar")
+                    .addItem(createCopyright())
+                    .create();
+
+
+            setContentView(aboutpage);
+        }
+
+        private Element createCopyright() {
+
+            Element copyright = new Element();
+            final String copyrightstring = String.format("@Copyright %d by Traffic Rules Awareness", Calendar.getInstance().get(Calendar.YEAR));
+            copyright.setTitle(copyrightstring);
+            copyright.setGravity(Gravity.CENTER);
+            copyright.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(ContactUs.this, copyrightstring, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            return copyright;
+        }
+
+
+
+        // Setting the Up button in Activity to return to More fragment
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+
+            super.onOptionsItemSelected(item);
+            switch (item.getItemId()) {
+                case android.R.id.home:
+                    finish();
+                    break;
+            }
+
+            return true;
+        }
 
 }
